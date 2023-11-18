4da4a7c3b66bb60764c26aa224c71ac137518c36
M-r-ReX <souvikhazra151@gmail.com>
2023-11-18 13:26:58 +0530

    added comments to the files.

--- countDown.js
+++ countDown.js
@@ -1,45 +1,71 @@
+// Initialize variables for given time and count time
 let givenTime = 0;
 let countTime = 0;
-function countDownStartEvent(){
+
+// Function to handle the start of the countdown
+function countDownStartEvent() {
+  // Set pauseEvent to true to indicate countdown is not paused
   pauseEvent = true;
-  if(givenTime ==0){
+
+  // Check if givenTime is 0, collect input values, convert to seconds, and start countdown
+  if (givenTime == 0) {
     let collectHr = document.querySelector('.collectHr');
     let collectMin = document.querySelector('.collectMin');
     let collectSec = document.querySelector('.collectSec');
-    givenTime = parseInt(collectHr.value)*3600 + parseInt(collectMin.value)*60 + parseInt(collectSec.value);
+
+    // Calculate total seconds from input values
+    givenTime = parseInt(collectHr.value) * 3600 + parseInt(collectMin.value) * 60 + parseInt(collectSec.value);
+
+    // Clear input fields
     collectHr.value = "";
     collectMin.value = "";
     collectSec.value = "";
+
+    // Start the countdown
     startCountDown(givenTime);
   }
 }
 
+// Variable to control whether the countdown is paused or not
 let pauseEvent = true;
-function pauseEventToggle(){
-  pauseEvent = !pauseEvent
-}
-function countDownPauseEvent(){
-  pauseEventToggle();
-}
-function startCountDown(givenTime){
-  startInterval(givenTime)
+
+// Function to toggle the pauseEvent variable
+function pauseEventToggle() {
+  pauseEvent = !pauseEvent;
 }
 
-let startInterval = (givenTime)=>{
+// Function to pause the countdown
+function countDownPauseEvent() {
+  pauseEventToggle();
+}
+
+// Function to initiate the countdown with the given time
+function startCountDown(givenTime) {
+  startInterval(givenTime);
+}
+
+// Function to start the countdown interval
+let startInterval = (givenTime) => {
   window.setInterval(() => {
-    if(pauseEvent){
-      if(givenTime!=countTime){
+    // Check if the countdown is not paused and there is time remaining
+    if (pauseEvent) {
+      if (givenTime != countTime) {
+        // Update the displayed countdown and decrement the remaining time
         flipAllCards(givenTime);
         givenTime--;
       }
     }
-  }, 1000)
+  }, 1000);
 }
+
+// Function to update the displayed countdown
 function flipAllCards(givenTime) {
+  // Calculate hours, minutes, and seconds from the remaining time
   const inputSeconds = givenTime % 60;
   const inputMinutes = Math.floor(givenTime / 60) % 60;
   const inputHours = Math.floor(givenTime / 3600);
 
+  // Update the displayed countdown
   flip(document.querySelector("[data-hours-tens]"), Math.floor(inputHours / 10))
   flip(document.querySelector("[data-hours-ones]"), inputHours % 10)
   flip(document.querySelector("[data-minutes-tens]"), Math.floor(inputMinutes / 10))
@@ -48,31 +74,40 @@ function flipAllCards(givenTime) {
   flip(document.querySelector("[data-seconds-ones]"), inputSeconds % 10)
 }
 
+// Function to animate the flipping of a card to display a new number
 function flip(flipCard, newNumber) {
-  const topHalf = flipCard.querySelector(".top")
-  const startNumber = parseInt(topHalf.textContent)
-  if (newNumber === startNumber) return
+  // Get the top and bottom halves of the flip card
+  const topHalf = flipCard.querySelector(".top");
+  const startNumber = parseInt(topHalf.textContent);
 
-  const bottomHalf = flipCard.querySelector(".bottom")
-  const topFlip = document.createElement("div")
-  topFlip.classList.add("top-flip")
-  const bottomFlip = document.createElement("div")
-  bottomFlip.classList.add("bottom-flip")
+  // If the new number is the same as the current number, do nothing
+  if (newNumber === startNumber) return;
 
-  top.textContent = startNumber
-  bottomHalf.textContent = startNumber
-  topFlip.textContent = startNumber
-  bottomFlip.textContent = newNumber
+  // Create elements for the top and bottom flips
+  const bottomHalf = flipCard.querySelector(".bottom");
+  const topFlip = document.createElement("div");
+  topFlip.classList.add("top-flip");
+  const bottomFlip = document.createElement("div");
+  bottomFlip.classList.add("bottom-flip");
 
+  // Set initial content for the top and bottom halves
+  top.textContent = startNumber;
+  bottomHalf.textContent = startNumber;
+  topFlip.textContent = startNumber;
+  bottomFlip.textContent = newNumber;
+
+  // Add event listeners for the start and end of the animation
   topFlip.addEventListener("animationstart", e => {
-    topHalf.textContent = newNumber
+    topHalf.textContent = newNumber;
   })
   topFlip.addEventListener("animationend", e => {
-    topFlip.remove()
+    topFlip.remove();
   })
   bottomFlip.addEventListener("animationend", e => {
-    bottomHalf.textContent = newNumber
-    bottomFlip.remove()
+    bottomHalf.textContent = newNumber;
+    bottomFlip.remove();
   })
-  flipCard.append(topFlip, bottomFlip)
+
+  // Append the top and bottom flips to the flip card
+  flipCard.append(topFlip, bottomFlip);
 }
--- script.js
+++ script.js
@@ -1,110 +1,107 @@
-// Stop Watch
+// Stopwatch Elements
 let startBtn = document.querySelector('#start');
 let stopBtn = document.querySelector('#stop');
 let resetBtn = document.querySelector('#reset');
 
-let hour = 0 , minute = 0 , second = 0 , count = 0;
+// Stopwatch Variables
+let hour = 0, minute = 0, second = 0, count = 0;
 let timer;
 
-// start btn
-startBtn.addEventListener('click',()=>{
+// Event listeners for stopwatch buttons
+startBtn.addEventListener('click', () => {
     timer = true;
     stopWatch();
-})
-// pause btn
-stopBtn.addEventListener('click',()=>{
-    timer = false;
-})
-// reset btn
-resetBtn.addEventListener('click',()=>{
-    timer = false;
-    hour =0; minute =0; second =0; count =0;
-    document.getElementById('hr').innerHTML = "00";
-    document.getElementById('min').innerHTML = "00";
-    document.getElementById('sec').innerHTML = "00";
-    document.getElementById('count').innerHTML = "00";
 });
 
-// contrilling with space btns the start and pause event
+stopBtn.addEventListener('click', () => {
+    timer = false;
+});
+
+resetBtn.addEventListener('click', () => {
+    timer = false;
+    hour = 0; minute = 0; second = 0; count = 0;
+    updateDisplay("00", "00", "00", "00");
+});
+
+// Controlling start and pause events with space bar
 let clickSpace = false;
 
-function toggleSpace(){
+function toggleSpace() {
     clickSpace = !clickSpace;
-    if(clickSpace == true){
+    if (clickSpace) {
         timer = true;
         stopWatch();
-    }else if(clickSpace == false){
+    } else {
         timer = false;
     }
 }
 
-document.addEventListener('keypress', (e)=>{
-    if(e.keyCode === 32){
+document.addEventListener('keypress', (e) => {
+    if (e.keyCode === 32) {
         toggleSpace();
     }
-   if(e.key === 'Enter'){
+    if (e.key === 'Enter') {
         timer = false;
-        hour =0; minute =0; second =0; count =0;
-        document.getElementById('hr').innerHTML = "00";
-        document.getElementById('min').innerHTML = "00";
-        document.getElementById('sec').innerHTML = "00";
-        document.getElementById('count').innerHTML = "00";
+        hour = 0; minute = 0; second = 0; count = 0;
+        updateDisplay("00", "00", "00", "00");
     }
+});
 
-})
-// Stop Watch main function
-function stopWatch(){
-    if(timer){
+// Stopwatch main function
+function stopWatch() {
+    if (timer) {
         count++;
-        if(count == 100){
+        if (count == 100) {
             second++;
             count = 0;
         }
-        if(second == 60){
+        if (second == 60) {
             minute++;
             second = 0;
         }
-        if(minute == 60){
+        if (minute == 60) {
             hour++;
-            minute =0;
-            second =0;
+            minute = 0;
+            second = 0;
         }
-        let hrString = hour;
-        let minString = minute;
-        let secString = second;
-        let countString = count;
-        if(hour<10){
-            hrString = "0" + hrString;
-        }
-        if(minute<10){
-            minString = "0" + minString;
-        }if(second<10){
-            secString = "0" + secString;
-        }
-        if(count<10){
-            countString = "0" + countString;
-        }
-        document.getElementById('hr').innerHTML = hrString;
-        document.getElementById('min').innerHTML = minString;
-        document.getElementById('sec').innerHTML = secString;
-        document.getElementById('count').innerHTML = countString;
+
+        // Formatting values to display with leading zeros
+        let hrString = hour < 10 ? "0" + hour : hour;
+        let minString = minute < 10 ? "0" + minute : minute;
+        let secString = second < 10 ? "0" + second : second;
+        let countString = count < 10 ? "0" + count : count;
+
+        // Update the display
+        updateDisplay(hrString, minString, secString, countString);
+
+        // Recursive call with a delay for smooth updating
         setTimeout(stopWatch, 10);
     }
 }
-// Flip animation js part
+
+// Function to update the display with new stopwatch values
+function updateDisplay(hr, min, sec, count) {
+    document.getElementById('hr').innerHTML = hr;
+    document.getElementById('min').innerHTML = min;
+    document.getElementById('sec').innerHTML = sec;
+    document.getElementById('count').innerHTML = count;
+}
+
+// Flip animation elements
 let rotate = document.querySelector('.flip-btn');
 let flipCard = document.querySelector('.flip-card');
 let flipCardInner = document.querySelector('.flip-card-inner');
 let flipCardBack = document.querySelector('.flip-card-back');
 let flipToggle = false;
 
-function flipToggleCntrl(){
+// Function to control the flip animation toggle
+function flipToggleCntrl() {
     flipToggle = !flipToggle;
     if (flipToggle) {
         flipCardInner.style.transform = 'rotateY(-180deg)';
         rotate.innerHTML = "Click Here For Stop Watch";
     } else {
         flipCardInner.style.transform = 'rotateY(0deg)';
-        rotate.innerHTML = "Click Here For Count Down Timer";
+        rotate.innerHTML = "Click Here For Countdown Timer";
     }
 }
